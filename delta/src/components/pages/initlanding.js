7d95292393230dbe85aabb630aba03d39b78d707
anvi-pen <91691176+anvi-pen@users.noreply.github.com>
2022-10-28 23:46:36 -0700

    Implements basic page switching
    Implements page switching between the landing page, the sign up page, and the log in page

--- delta/src/components/pages/initlanding.js
+++ delta/src/components/pages/initlanding.js
@@ -1,11 +1,31 @@
-import {BrowserRouter, Route} from "react-router-dom";
+import {BrowserRouter, Route, Routes, Link} from "react-router-dom";
+import SignUp from "./signup"
+import LogIn from "./login"
 
 function InitLanding()
 {
     return (
         <BrowserRouter>
-            <Route path="./components/pages/signup" />
+            <Routes>
+                <Route exact path = "/" element={<Landing />} />
+                <Route path = "/signup" element={<SignUp />} />
+                <Route path = "/login" element={<LogIn />} />
+            </Routes>
         </BrowserRouter>
+    );
+}
+
+function Landing()
+{
+    return (
+        <div>
+            <div>
+                <Link to="/signup">Sign Up</Link>
+            </div>
+            <div>
+                <Link to="/login">Log In</Link>
+            </div>
+        </div>
     );
 }
 
---
+++ delta/src/components/pages/signup.js
@@ -0,0 +1,10 @@
+function SignUp()
+{
+    return (
+        <div>
+            signup
+        </div>
+    );
+}
+
+export default SignUp;
