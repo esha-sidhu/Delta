44db185d5c58ccfddffc3af60b6d5f8f828bd37d
ekuczynski <109834726+ekuczynski@users.noreply.github.com>
2022-11-24 23:38:07 -0800

    To-Do List handles checked-off items and archive Once the user checks off items, they remain in their list so that they can visually see their progress. However, if they want to clear up their to-do list or their list is at the maximum of 10 tasks and they want to add more, they can click the 'Update Archive' button which removes the checked-off items from their current list and moves them to their archived list.

--- delta/src/components/pages/todo-list.js
+++ delta/src/components/pages/todo-list.js
@@ -4,57 +4,80 @@ import React, {useEffect, useState} from 'react';
 function ToDoList()
 {
     const [todoitems, setToDoItems] = useState([]);
-    const taskCount = useState(0);
+    const [numOfTasks, setNumOfTasks] = useState(0);
     const taskMax = 10;
-
+    const [archivedTasks, setArchivedTasks] = useState([]);
 
     function handleTask()
     {
         let val = document.getElementById("taskValue").value;
-        let tempTodoitems = [];
         
-        if (todoitems.includes(val) === false && val !== '') {
-            for (let i = 0; i < todoitems.length; i++)
-            {
-                tempTodoitems.push(todoitems[i]);
-            }
-            tempTodoitems.push(val);
-            setToDoItems(tempTodoitems); 
+        // adds inputed task to the to-do list if not a repeat and there
+            // are less than the max number of tasks
+        if (todoitems.includes(val) === false && val !== '' 
+            && numOfTasks < taskMax) 
+        {   
+            todoitems.push(val);
+            setToDoItems(todoitems);
+            setNumOfTasks(numOfTasks + 1);
+
+            console.log(numOfTasks);
             console.log(todoitems);
         }
-        document.getElementById("taskValue").value = ''; 
+        document.getElementById("taskValue").value = ''; // clears input box
+    }
+
+    function handleCheck(val)
+    {
+        // TODO: can update to do strikethrough/other styling here later
+        console.log("checked");
     }
 
     function handleDelete(val)
     {
-        // TODO: fix bug where X button doesn't render button click until second press
-        // explanation: this only happens when the user first enters an element. Once the
-        // element is added, any X button click will not be registered but any other
-        // button click after that does (so only the very very first button click after adding
-        // a new item won't get registered, weird huh)
-
-        // this is also a bug with the list because when an item is added, the console
-        // log doesn't get the updated list until the next action is made (which is weird
-        // because the list on the website visually does get updated)
-
         var copiedList = [];
         const indexOfClick = todoitems.indexOf(val);
 
+        // copies all except the 'X' task from the current to-do list
+            // into a new list (essentially removing the one task)
         for (let i = 0; i < todoitems.length; i++)
         {
             if (i !== indexOfClick)
             {
                 copiedList.push(todoitems[i]);
             }
-            console.log(copiedList);
-            setToDoItems(copiedList);
         }
+        setToDoItems(copiedList);
+        setNumOfTasks(numOfTasks - 1);
+    }
 
-        console.log(val);
-        console.log(todoitems.includes(val));
-        
-        // idea: implement by making there max of 10 buttons and then
-        // just index the value with the button's
+    function updateArchive() 
+    {
+        var tempList = [];
+        var checkboxes = document.querySelectorAll("input[type=checkbox]");
+        var newNumOfTasks = 0;
+        console.log(numOfTasks);
+
+        // remove "checked" to-do list tasks from the list
+        // add "checked" to-do list tasks to the archive list
+        for (let i = 0; i < checkboxes.length; i++)
+        {
+            if (checkboxes[i].checked === true) // archive list
+            {
+                archivedTasks.push(todoitems[i]);
+            }
+            if (checkboxes[i].checked === false) // current to-do list
+            {
+                tempList.push(todoitems[i]);
+                newNumOfTasks = newNumOfTasks + 1;
+            }
+        }
+        setArchivedTasks(archivedTasks);
+        setToDoItems(tempList);
+        setNumOfTasks(newNumOfTasks);
+
+        console.log(archivedTasks);
+        console.log(numOfTasks);
     }
 
 
@@ -74,6 +97,8 @@ function ToDoList()
                 </font>
                 <font size="1">
                     Only add up to 10 items to help with stress management.
+                    <br></br>
+                    To send your checked-off tasks to your completed archive, and remove them from your current list, click 'Update Archive' below.
                     <br></br><br></br>
                 </font>
                 <div id="newItem">
@@ -86,12 +111,14 @@ function ToDoList()
                 {todoitems.map((val) => {
                     return (
                         <div key={val}>
-                            <input type="checkbox"></input>
+                            <input type="checkbox" onClick={() => handleCheck(val)}></input>
                             {val} &ensp;
                             <button type="button" onClick={() => handleDelete(val)}> X </button>
                         </div>
                     );
                 })}
+                <br></br>
+                <button type="button" onClick={updateArchive}>Update Archive</button>
             </ol>
         </div>
     );
