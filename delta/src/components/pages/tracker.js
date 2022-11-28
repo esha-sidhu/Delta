9be974e3e3c856528b18198f47de254272e41f07
Snigdha Kansal <snigdha0206@g.ucla.edu>
2022-11-27 21:20:27 -0800

    separates links for trackers

--- delta/src/components/pages/tracker.js
+++ delta/src/components/pages/tracker.js
@@ -12,8 +12,11 @@ function Tracker()
             </div>
             <div>
                 <Link to ="/sleeptracker">Go to Sleep Tracker</Link>
+                <br/>
                 <Link to="/WaterTracker">Go to Water Tracker</Link>
+                <br/>
                 <Link to="/MoodTracker">Go to Mood Tracker</Link>
+                <br/>
             </div>
             <div>
                 <Link to="/board">Go back to your bulletin board</Link>
