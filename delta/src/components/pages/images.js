ef2c17c28dd8cf75aacdbaa98e5581221d0b6fe2
ekuczynski <109834726+ekuczynski@users.noreply.github.com>
2022-11-29 18:45:23 -0800

    Styled Image Search Page I styled the image search page so that the image layout is easier to view when adding images to the board.

--- delta/src/components/pages/images.js
+++ delta/src/components/pages/images.js
@@ -1,4 +1,3 @@
-// import Unsplash, { toJson } from 'unsplash-js';
 import { createApi, search } from 'unsplash-js';
 import React, { useState, useEffect } from "react";
 import {addDoc, collection, getDocs, query, where, updateDoc, getFirestore, doc} from 'firebase/firestore';
@@ -81,27 +80,25 @@ function SearchImages() {
         window.location.assign('/board');
     }
 
-    /*
-    const serverApi = createApi({
-        accessKey: 'qcUUfkIE6ckQyfsfpMcdCkPDPwD06w89UOZIZQmQVjM'
-      });
-    */
     const browserApi = createApi({
         apiUrl: 'https://mywebsite.com/unsplash-proxy'
     });
 
     const unsplash = createApi({
         accessKey: 'qcUUfkIE6ckQyfsfpMcdCkPDPwD06w89UOZIZQmQVjM',
-        // headers: { 'X-Custom-Header': 'foo' },
       });
 
   return (
-    <div>
-        <input 
+    <div className='imageWrapper'>
+        <div className='imageTitle'>
+          &ensp;&ensp;&ensp;IMAGE SEARCH&ensp;&ensp;&ensp;
+        </div>
+        <input className='inputText'
             id='search'
             type="text"
         />
-        <button type="button" onClick={HandleClick}>Search</button>
+        <button type="button" onClick={HandleClick} className='buttonSearch'>Search</button>
+        <br></br><br></br>
         {image.map((val, i) => {
             return(
               <div>
@@ -109,29 +106,13 @@ function SearchImages() {
                   <img id={"imageInsideBox" + i} src={val.image}></img>
                 </button>
                 <br></br>
-                <small>Author: {val.authorName}</small>
-                <br></br>
+                <small className='author'>Author: {val.authorName}</small>
+                <br></br><br></br>
               </div>
             );
         })}
     </div>
   );
 }
-
-/*
-const unsplash = new Unsplash({
-  applicationId: "qcUUfkIE6ckQyfsfpMcdCkPDPwD06w89UOZIZQmQVjM",
-  secret: "QJ-iFW0XxUocomXIwCw1DoGhew4R4iy8-hYMi0053V8"
-});
-
-function searchImages() {
-    unsplash.search.photos("cats", 1)
-  .then(toJson)
-  .then(json => {
-    console.log("Image Object: ");
-    console.log(json);
-  });
-}
-*/
 
 export default SearchImages;
